1b5ab9af67cfd6ca02ba75ac734ed73a16b7ee0c
cubipod <jupiterisopod@gmail.com>
2025-01-07 23:00:06 -0500

    Add TMUX

--- dnf-install-packages.sh
+++ dnf-install-packages.sh
@@ -4,7 +4,7 @@ echo "Installing packages..."
 
 # This is every single relevant program related to the dotfiles
 # (Eg. i3, alacritty, etc.)
-sudo dnf install i3 polybar -y
+sudo dnf install i3 polybar tmux -y
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 echo "Installation successful."
